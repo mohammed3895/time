c355530b2166fff47bcbcedeb843aee35c3c2993
mohammed sameer <85784627+mohammed3895@users.noreply.github.com>
2024-06-12 21:41:09 +0300

    Update NavItems.tsx

--- src/components/shared/navbar/NavItems.tsx
+++ src/components/shared/navbar/NavItems.tsx
@@ -3,12 +3,16 @@ import Navlink from "./Navlink";
 import StopWatch from "../StopWatch";
 import Timer from "../clock/Timer";
 
-const NavItems = ({ onClick }: { onClick: () => void }) => {
+const NavItems = ({ onClick }: { onClick?: () => void }) => {
   return (
     <div className="w-full md:w-[300px] flex flex-col md:flex-row items-center justify-between gap-3">
       <div className="flex items-start md:items-center md:justify-between justify-start gap-3 flex-col md:flex-row w-full">
         {NAVITEMS.map((item) => (
-          <Navlink key={item.href} item={item} onClick={onClick} />
+          <Navlink
+            key={item.href}
+            item={item}
+            onClick={onClick ? onClick : () => {}}
+          />
         ))}
         <StopWatch />
         <Timer initialSeconds={0} />
